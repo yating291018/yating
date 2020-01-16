ce0cf146257e5ad3588df653433539152bfe7863
hugang1 <hugang1@tuhu.cn>
2020-01-16 20:05:37 +0800

    第一版

---
+++ index.js
@@ -0,0 +1,83 @@
+
+import {
+  Button
+} from 'view-design'
+// Icon,
+// ButtonGroup,
+// Link,
+// Card,
+// Input,
+// Row,
+// Col,
+// Layout,
+// Content,
+// Header,
+// Sider,
+// Footer,
+// Form,
+// FormItem,
+// Divider,
+// InputNumber,
+// Select,
+// OptionGroup,
+// Option,
+// CellGroup,
+// Cell,
+// Split,
+// Switch,
+// Tag,
+// Scrollbar,
+// Collapse,
+// CollapseItem,
+// CollapseTransition,
+// Cascader,
+// Alert,
+// Checkbox,
+// CheckboxGroup,
+// CheckboxButton,
+// Radio,
+// RadioGroup,
+// RadioButton,
+// Modal,
+// Tabs,
+// TabPane,
+// TimePicker,
+// DatePicker,
+// BreadcrumbItem,
+// Breadcrumb,
+// Dropdown,
+// DropdownItem,
+// DropdownMenu,
+// Menu,
+// MenuGroup,
+// MenuItem,
+// Submenu,
+// Table,
+// TableColumn,
+// Pagination,
+// Circle,
+// Progress,
+// Badge,
+// Tooltip,
+// Slider,
+// Poptip,
+// Drawer,
+// Upload,
+// Step,
+// Steps,
+// Avatar,
+// AutoComplete,
+// Tree,
+// ColorPicker
+
+// const components = {
+//   'yt-button': Button
+// }
+
+const install = (Vue) => {
+  Vue.component('yt-button', Button)
+}
+
+export default {
+  install
+}
